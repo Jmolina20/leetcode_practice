3fffa0f02aee238b96656c9e35c2cb4453925863
Jmolina20 <julianmolina2011@gmail.com>
2022-07-06 16:00:00 -0400

    first file problem with intro text

---
+++ sum_of_1d_array.js
@@ -0,0 +1,15 @@
+//Given an array nums. We define a running sum of an array as 
+//runningSum[i] = sum(nums[0]…nums[i]).
+
+//Return the running sum of nums
+
+
+class Solution {
+    public int[] runningSum(int[] , nums) {
+        
+        for (int x = 0; x < sum.length ; x++){
+            
+        }
+
+    }
+}
